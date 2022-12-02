d14d41131aaaba12d0f6150d4bad00d85604acdd
Pham Anh Tuan <phamanhtuan01789@gmail.com>
2022-12-02 11:18:38 +0700

    thankyou ui done

--- src/components/ThankYou/ThankYou.js
+++ src/components/ThankYou/ThankYou.js
@@ -1,18 +1,38 @@
+import { faGifts, faHouse } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Images from '~/assets/images';
+import { Link } from 'react-router-dom';
+import Plants from '../Plants';
+import config from '~/config/routes';
 import styles from './ThankYou.module.scss';
 
 function ThankYou() {
     return (
         <div className={styles.wrapper}>
             <div className={styles.container}>
-                {/* <div className={styles.plant}>
+                <div className={styles.plant1}>
                     <Plants />
-                </div> */}
+                </div>
+                <div className={styles.plant2}>
+                    <Plants />
+                </div>
+
                 <div className={styles.card}>
                     <img src={Images.Thank} alt="thank" />
                     <div className={styles.content}>
                         <h1>Thank you</h1>
                         <span>In the bottom of heart, we would like to express our huge thanks for your support </span>
+                        <div className={styles.btns}>
+                            <Link to={config.home} className={styles.backBtn}>
+                                <FontAwesomeIcon icon={faHouse} />
+                                Back to home
+                            </Link>
+                            <Link to={config.product} className={styles.backBtn}>
+                                <FontAwesomeIcon icon={faGifts} />
+                                Continue shopping
+                            </Link>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/pages/Products/ListItems.js
+++ src/pages/Products/ListItems.js
@@ -36,23 +36,25 @@ function ListItem({ items }) {
                     productList.map((item, index) => {
                         return (
                             <li className={styles.item} key={index}>
-                                <img src={item.imageUrl} alt={item.imageUrl} />
-                                <div className={styles.menu_link_widget}>
-                                    <Widget icon={faMagnifyingGlass} id={item.id} />
-                                    <Widget icon={faCartShopping} addOne={item} />
-                                    <Widget icon={faHeart} />
-                                </div>
-                                <h6>
-                                    <Link to={`/products/${item.id}`} className={styles.item_name}>
-                                        {item.name}
-                                    </Link>
-                                </h6>
-                                <p>
-                                    <Link
-                                        to={`/products/${item.id}`}
-                                        className={styles.item_price}
-                                    >{`$ ${item.price}.00`}</Link>
-                                </p>
+                                <Link to={`/products/${item.id}`}>
+                                    <img src={item.imageUrl} alt={item.imageUrl} />
+                                    <div className={styles.menu_link_widget}>
+                                        <Widget icon={faMagnifyingGlass} id={item.id} />
+                                        <Widget icon={faCartShopping} addOne={item} />
+                                        <Widget icon={faHeart} />
+                                    </div>
+                                    <h6>
+                                        <Link to={`/products/${item.id}`} className={styles.item_name}>
+                                            {item.name}
+                                        </Link>
+                                    </h6>
+                                    <p>
+                                        <Link
+                                            to={`/products/${item.id}`}
+                                            className={styles.item_price}
+                                        >{`$ ${item.price}.00`}</Link>
+                                    </p>
+                                </Link>
                             </li>
                         );
                     })}
